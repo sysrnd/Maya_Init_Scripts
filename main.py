695573bb6ac325661b1156800c73c4b4ffe08ab2
arturoalcibia <arturoalcibia@hotmail.com>
2018-02-05 20:37:41 -0600

    first beta menuBrowser

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import maya.cmds as cmds
 import maya.mel as mel
-mel.eval('optionVar -iv "isShelfSave" 0')
+
+import Maya_Init_Scripts.menuBrowser
+
+	
--- menuBrowser.py
+++ menuBrowser.py
@@ -1,117 +1,143 @@
 #TODO
-#scan for folders for menus
-#scan for subfolders for submenus
-#find those that actually have the app.py file
-#check if number of apps found match with those, it they do skip steps
-#write to a file
-#TODO
 #command
-#json
- myimport os
-import Utils.findEnv as findEnv
+#title
+#filter by user
+#always return full path
 
-class proceduralMenus (object) :
-	
+import os
+import maya.cmds as cmds
+import Utils.os_Find_Env.findEnv_app as findEnv
+import getpass
+
+class proceduralMenus(object):
 	def __init__(self):
+		'''
+		Initiliazes env vars of MAYA_SCRIPT_PATH
+		Creates a list of folders where it shouldn't look into
 
-		self.invalidFolders = ['initScripts']
-		self.env = findEnv.findEnv_()
+		'''
+		self.invalidFolders = ['Maya_Init_Scripts', '.git', 'Utils', 'Modules']
+		self.invalidFiles = ['__init__', '.pyc', 'old']
+		self.env = findEnv.findEnvVar_()
+		
 		self.menus = []
 		self.submenus = []
-	def main(self):
 
-		avMenus = self.browseDirs
+	def main(self):
+		avMenus = self.browseDirs(self.env)
 
 		for avMenu in avMenus:
-			if self.isValidMenu(avMenu):
-				self.createMenu()
+			#concatenate path
+			avMenuPath = self.env + '/' + avMenu
 
-				avSubmenus = browseFiles(avMenu)
+			if self.isValidMenu(avMenuPath):
+				menu = self.createMenu(avMenu)
+				avSubmenus = self.browseDirs(avMenuPath)
+
 				for avSubmenu in avSubmenus:
-					if self.isValidSubMenu(avSubmenu)
-						self.createSubMenu()
+					avSubmenuPath = avMenuPath + '/' + avSubmenu
+					filesInSubmenus = self.browseFiles(avSubmenuPath)
+					for avFileInSubmenu in filesInSubmenus:
+						filesInSubmenuPath = avSubmenuPath + '/' + avFileInSubmenu
+						if self.isValidSubMenu(filesInSubmenuPath):
+							command = self.parseCommandString(filesInSubmenuPath)
+							self.createSubMenu(menu, avFileInSubmenu, command)
 
 
 
 
+	def browseDirs(self, path):
+		'''
+		'''
+		menus = []
 
-    def browseDirs(self, path):
-    	'''
-    	'''
-    	menus = []
-
-    	for dirs in os.listdir(path):
-			if os.path.isdir(dirs):
-				menu = cmds.menu(dirs, label=dirs, parent='MayaWindow')
-				menus.append(menu)
+		for dirs in os.listdir(path):
+			dirPath = path + '/' + dirs
+			if os.path.isdir(dirPath):
+					menus.append(dirs)
 
 		return menus
 
-	def createMenu(self, dir, command):
-		'''
-		'''
-		menu = cmds.menu(dirs, label=dirs, parent='MayaWindow')
 
-	def createSubMenu(self, menuParent, subMenu):
+	def browseFiles(self, path):
 		'''
 		'''
+		files = []
 
-    def browseFiles(self, menu, path):
-    	'''
-    	'''
-    	files = []
-
-    	for file in os.listdir(path):
-			files.append(file)
+		for file in os.listdir(path):
+			filePath = path + '/' + file
+			if os.path.isfile(filePath):
+				files.append(file)
 
 		return files
 
-	def isValidMenu(file):
+	def isValidMenu(self, file):
 		'''
 		'''
 		isValid = False
 
-		if os.path.isDir(file):
+		#unnecesary
+		if os.path.isdir(file):
 			isValid = True
 
-		if file in self.invalidFolders:
-			isValid = False
+		for invalid in self.invalidFolders:
+			if file.find(invalid) != -1:
+				isValid = False
 
 		return isValid
-
+	
 	def isValidSubMenu(self, file):
 		'''
 		'''
+		fileName = file.split('/')[-1]
+
+
 		isValid = False
+		#unnecesary
+		if os.path.isdir(file):
+			isValid = False
 
-		if os.path.isfile(file):
+		if file.find('app') != -1 and file.find('old') == -1:
 			isValid = True
 
-		if file.find('app') != -1:
-			isValid = True
-			if file.find('old'): == -1:
-				#in case there are several apps
-				isValid = True
-			else:
+		for invalid in self.invalidFiles:
+			if file.find(invalid) != -1:
 				isValid = False
 
 		return isValid
 
-    def writeLocalInfo(self, file, txt):
-    	'''
-    	'''
+	def createMenu(self, direc):
+		'''
+		'''
+		menu = cmds.menu(direc, label=direc, parent='MayaWindow')
+		return menu
 
-        with open(file + '.txt' ,'w') as f:
-            data = f.write(txt)
+	def createSubMenu(self, menuParent, subMenu, command):
+		'''
+		'''
+		subMenu = cmds.menuItem(subMenu, label=subMenu, parent= menuParent, command=command)
+		return subMenu
 
-    def readLocalInfo(self, file, txt):
-    	'''
-    	'''
 
-        if os.path.exists(file + '.txt'):
-            with open(file + '.txt' ,'r') as f:
-                data = f.read()
-            txt.setText(data)
-            return True
-        else:
-            return False
+	def parseCommandString(self, path):
+		'''
+		'''
+		relativePath = path.replace(self.env + '/', '')
+		relativePath = relativePath.replace('/', '.')
+		parsedPath = 'import ' + relativePath + '; ' + 'reload(' + relativePath + ')'
+		return parsedPath
+
+	def splitLabelScreen(self, file):
+
+		label = file.split('.')[0]
+		label = label.strip('app')
+		
+		return label
+
+	def getFileName(self, path):
+
+		file = path.split('/')[-1]
+		return file
+
+menu = proceduralMenus()
+menu.main()
