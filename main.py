f44f75e16faa1eee8568c957365af579593e897b
arturoalcibia <arturoalcibia@hotmail.com>
2018-05-20 19:35:12 -0500

    added import statement of shelf to main

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 import Maya_Init_Scripts.menuBrowser
-print 'ManBearPig'
-=======
-import maya.cmds as cmds
-import maya.mel as mel
-
-import Maya_Init_Scripts.menuBrowser
->>>>>>> 1a32d9f1f5d83f64df3840140a240372d9e3418b
+import Maya_Init_Scripts.shelfCharTools
